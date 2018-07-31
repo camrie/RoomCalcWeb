282c6b2b641a65d8e5b6b5311d167960a8079e4f
Camrie Madsen <camrie.madsen@aexp.com>
2018-07-31 08:55:11 -0700

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# RoomCalcWeb
---
+++ calculator.js
@@ -0,0 +1,25 @@
+const internet = 60.0;
+const pet_rent = 35.0;
+//var rent_util = document.getElementById("rent_util").value;
+//var electricity = document.getElementById("elec").value;
+var numRoommates = 2.0;
+
+var calc = function() {
+    let rent_util = document.getElementById("rent").value;
+    let electricity = document.getElementById("elec").value;
+    console.log(rent_util);
+    console.log(electricity);
+    // if(rent_util.trim() == '' || electricity.trim() == '') {
+    //     return "Please input a number"
+    // }
+    // else {
+        let rent_each = (rent_util - pet_rent) / numRoommates;
+        let int_each = internet / numRoommates;
+        let elec_each = electricity / numRoommates;
+        var kev_total = rent_each + elec_each - int_each;
+        console.log(kev_total);
+        document.getElementById("value").innerHTML = kev_total;
+      //  document.getElementById("value").innerHTML = "test changed";
+        return kev_total;
+    // }
+}
